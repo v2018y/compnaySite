7354c1d59dd01eb39080dd12cffefe9ce64f45dc
Naga Durga Pratyusha <durga@124apps.com>
2019-11-06 11:06:20 +0530

    0.1.3: ReModify the file structure uisng php

---
+++ product.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>V & Y | About US</title>
+    <?php include('./data/head/Head.php'); ?>
+</head>
+
+<body>
+    <header>
+    <?php include('./data/navbar/NavBar.php'); ?>
+    </header>
+
+<!-- footer -->
+<?php include("./data/footer/Footer.php") ?>
+<!-- //footer -->
+</body>
+</html>
