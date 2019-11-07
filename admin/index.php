beafe761ed2cdaa2b69df81cc2a81539c776f6d9
Vishvanath Surwashe <vsurwshe@gmail.com>
2019-11-07 11:29:41 +0530

    0.1.5: Product Image Suceessfully

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,74 @@
+<?php
+   include("../data/db/db.php");
+  // If upload button is clicked ...
+  if (isset($_POST['upload'])) {
+  	// Get image name
+	$image_name = $_FILES['image']['name'];
+    // convert into blob 
+	$image_bolob = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+	// Query for executions  
+	$sql = "INSERT INTO `product`(`mime`, `data`, `url`) VALUES ('$image_name', '$image_bolob','demo')";
+  	// execute query
+	$result = mysqli_query($link, $sql);
+	if($result){
+		echo "Your Data is Inserted SuccessFully";
+		header("Location: index.php");
+	}else{
+		echo "<script>alert('Your Data is Not Inserted SuccessFully')</script>";
+	 }
+}
+?>
+<!DOCTYPE html>
 <html>
-<body>
-		
-<form method="POST" action="upload.php" enctype="multipart/form-data">
- <input type="file" name="myimage">
- <input type="submit" name="submit_image" value="Upload">
-</form>
+<head>
+<title>Image Upload</title>
+<style type="text/css">
+   #content{
+   	width: 50%;
+   	margin: 20px auto;
+   }
+   form{
+   	width: 50%;
+   	margin: 20px auto;
+   }
+   form div{
+   	margin-top: 5px;
+   }
+   .row {
+  	display: flex;
+	}
 
+/* Create three equal columns that sits next to each other */
+.column {
+  flex: 33.33%;
+  padding: 5px;
+}
+</style>
+</head>
+<body>
+<div id="content">
+  <?php
+   $result = mysqli_query($link, "SELECT * FROM `product`");
+   echo "<div class='row'>";	
+   while ($row = mysqli_fetch_array($result)) {
+	   echo "<div class='column'>";
+      	echo "<img  style='width:100%' src='data:image/jpeg;base64,".base64_encode($row['data'])."'>";
+		  echo "<p>".$row['mime']."</p>";
+		  echo "</div>";
+	}
+	echo "</div>";
+  ?>
+  <form method="POST" enctype="multipart/form-data">
+  	<div>
+  	  <input type="file" name="image">
+  	</div>
+  	<div>
+      <input  id="text" name="image_url"  placeholder="Enter Url Data"></textarea>
+  	</div>
+  	<div>
+  		<button type="submit" name="upload">Submit Product Image</button>
+  	</div>
+  </form>
+</div>
 </body>
 </html>
--- data/db/db.php
+++ data/db/db.php
@@ -1,12 +1,13 @@
 <?php
-  $link = mysqli_connect("localhost", "root", "", "santoshsir");
+  $link = mysqli_connect("172.19.0.3", "root", "root");
   if (!$link) {
       die("Database connection failed: " . mysqli_connect_error());
   }
-
- $db_select = mysqli_select_db($link, "santoshsir");
-  if (!$db_select) {
+  $db = mysqli_select_db($link, "santoshsir");
+  if (!$db) {
       die("Database selection failed: " . mysqli_connect_error());
   }
+  echo "Database Connections Successfully......";
   mysqli_set_charset($link, 'UTF8');
-?>
+?>  
+
